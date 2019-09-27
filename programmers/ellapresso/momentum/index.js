faaeda8e921b91dfd35ce5377503057cae82375e
ellapresso <ellapresso@gmail.com>
2019-09-27 21:53:33 +0900

    모멘텀 추가작업중

--- programmers/ellapresso/momentum/index.js
+++ programmers/ellapresso/momentum/index.js
@@ -1,25 +1,36 @@
 const clock = document.querySelector('#clock');
 const KEY = '7817175-59009f7cca8be8133837e7e46';
-const APIKEY = 'https://pixabay.com/api/?key=' + KEY + '&q=black+blue+cat&image_type=photo';
+const APIKEY = 'https://pixabay.com/api/?key=' + KEY + '&q=blue+black+cats&image_type=photo';
 const body = document.querySelector('body');
 const input = document.querySelector('#input');
 const form = document.querySelector('form');
 const listBox = document.querySelector('#listBox');
 const toDos = [];
+let number = 0;
 
 function setTimer() {
-    setTimeout(getImages, 5000);
     getImages()
+    setInterval(getImages, 3000);
+}
+
+function oneToTwenty(){
+    console.log(number)
+    number++
+    if(number===20){
+        number=0
+    }
 }
 
 function getImages() {
+    oneToTwenty()
     fetch(APIKEY)
         .then(function (response) {
             return response.json();
         })
         .then(function (json) {
-            body.style.backgroundImage = `url(${json.hits[1].largeImageURL})`;
+            body.style.backgroundImage = `url(${json.hits[number].largeImageURL})`;
             body.style.backgroundSize = 'cover';
+            console.log(json)
         }).catch((err) => {
             body.style.background = 'black'
             console.log(err)
@@ -50,24 +61,31 @@ function setLocal(toDos) {
 
 function showList() {
     const history = localStorage.getItem('toDo');
-    JSON.parse(history).forEach(e => {
-        appendTxt(e)
+    JSON.parse(history).forEach((e,i) => {
+        appendTxt(e,i)
         toDos.push(e)
     });
 }
 
-function appendTxt(txt) {
+function appendTxt(txt,idx) {
     const li = document.createElement("li");
     const span = document.createElement("span");
     listBox.appendChild(li)
+    li.id='num'+idx
+    li.ondblclick =  () => {done(idx)}
+    // li.onclick = () =>{del(idx)}
     li.appendChild(span);
     span.innerText = txt;
 }
 
-function done() {
-    listBox.addEventListener('click', function () {
-        console.log(this)
-    })
+function done(idx) {
+    const txt = listBox.querySelector('#num'+idx).querySelector('span')
+    txt.style.textDecoration = (txt.style.textDecoration === 'line-through')?'':'line-through'
+
+}
+
+function del(idx){
+    console.log(idx)
 }
 
 function init() {
@@ -76,7 +94,7 @@ function init() {
     setInterval(getTime, 1000);
     form.addEventListener('submit', saveLocal)
     showList()
-    done()
+    oneToTwenty()
 }
 
 init();
